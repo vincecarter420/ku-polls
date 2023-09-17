ab8415ad3f20a4090a02c12b55bf0a3b3f70ad57
Natthadit LERTPISANWUT <natthadit.l@ku.th>
2023-09-17 18:20:00 +0700

    fix some code

--- polls/models.py
+++ polls/models.py
@@ -33,7 +33,7 @@ class Choice(models.Model):
     @property
     def votes(self):
         '''count the votes for this choice.'''
-        return self.vote_set.count
+        return self.vote_set.count()
 
     def __str__(self):
         return self.choice_text
@@ -42,6 +42,3 @@ class Vote(models.Model):
     """Record a choice for a question made by a user."""
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
-
-    def __str__(self):
-        pass
